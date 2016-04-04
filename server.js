8989229a327807d62fcbc0ad35683a100a6caa47
mcmenemy <mr_mcmenemy@yahoo.com>
2016-04-04 13:23:28 -0700

    attempt to change server.js for heroku deploy

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('./config/express');
 var app = express();
 
-app.listen(3000);
-module.exports = app;
+var server = app.listen(process.env.PORT || 3000, function () {
+  console.log('Server listening on port ' + (process.env.PORT || 3000) + '...');
+});
 
-console.log('Server running at http://local-host:3000/');
+module.exports = app;
