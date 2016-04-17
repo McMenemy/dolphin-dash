df31a047d4488203114ce9177e242d633f6e179c
mcmenemy <mr_mcmenemy@yahoo.com>
2016-04-17 12:16:15 -0700

    temp remove DB

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var mongoose = require('./config/mongoose');
+// var mongoose = require('./config/mongoose');
 var express = require('./config/express');
 
-var db = mongoose();
+// var db = mongoose();
 var app = express();
 
 app.listen(process.env.PORT || 3000, function () {
