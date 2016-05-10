b2e11ed6d7d58eb12dd507fab35a7d0b2d3ba495
mcmenemy <mr_mcmenemy@yahoo.com>
2016-05-10 11:58:31 -0400

    change name to microbe-munch

--- frontend/components/background.jsx
+++ frontend/components/background.jsx
@@ -16,7 +16,7 @@ var Background = React.createClass({
         <h2>Background Story</h2>
 
         <p className='background'>
-          You are a microorganism trying to make your way in the big ocean by munching on smaller microorganisms that swarm the seas. Your prey will not allow their demise easily and it will take some skillful maneuvering to catch them. However, take care not to narrow you focus too much because sometimes an even larger microorganism will patrol these waters and you may become the prey not the predator... One last warning - as the night approaches differentiating between friend and foe may not be easy.
+          You are a microorganism trying to make your way in the big ocean by munching on smaller microorganisms that swarm the seas. Your prey will not allow their demise easily and it will take some skillful maneuvering to catch them. However, take care not to narrow you focus too much because sometimes an even larger microbes will patrol these waters and you may become the prey not the predator... One last warning - as the night approaches differentiating between friend and foe may not be easy.
         </p>
 
         <h2>Directions</h2>
